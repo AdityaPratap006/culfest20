0ad74c9eab3c14ff3ee8d86059e6304918466ab7
Aditya Pratap <adityapratap7344@gmail.com>
2019-12-23 09:34:26 +0530

    commit 2

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.module.scss';
+
+
+export class Home extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>CULFEST 20!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
+
